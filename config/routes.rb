061d914be62ec5e6b694f7fe8d824e5976956e4e
Thomas Stachl <thomas@stachl.me>
2013-02-08 13:43:53 +0100

    current migrations routes, need change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 DeskToolbelt::Application.routes.draw do
+  scope 'migrations', as: 'migrations' do
+    match 'desk' => 'migrations#desk'
+    match 'zendesk' => 'migrations#zendesk'
+    match 'select' => 'migrations#select'
+    match 'mapping' => 'migrations#mapping'
+    match 'finish' => 'migrations#finish'
+    root to: redirect("/migrations/desk")
+  end
+
   resources :auths do
     get 'change', on: :member
   end
